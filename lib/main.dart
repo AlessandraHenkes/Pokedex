8a9a6848593f88f72ea2d99b6b236f25a0bb7626
Alessandra <alessandra.henkes@cwi.com.br>
2021-12-20 16:00:25 -0300

    Adicionando estrutura inicial do projeto

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Pokédex',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const Scaffold(
+        backgroundColor: Colors.blue,
+      ),
+    );
+  }
+}
